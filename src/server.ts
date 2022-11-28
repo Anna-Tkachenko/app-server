8a54faa06b491646f6cb9b00dc1251809ea5c780
annatkachenko <tkachenko.anna835@gmail.com>
2022-11-28 19:48:02 +0200

    server refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-
-import { router as goodsRouter} from './routes/goods';
-import { router as colorsRouter} from './routes/colors';
 import serverless from 'serverless-http';
+import * as goodController from "./controllers/goods";
+import * as colorsController from "./controllers/colors";
+
+const router = express.Router();
 
 const app = express();
 
-app.use(cors());
-app.use('/goods', express.json(), goodsRouter);
-app.use('/colors', colorsRouter);
 
-app.listen(5000);
+router.get('/', (req, res) => {
+    res.json({bob: 1})
+})
+
+app.use(cors());
+
+router.get('/goods/', goodController.getAll)
+router.get('/goods/:goodId', goodController.getOne)
+router.post('/goods/', goodController.add);
+router.delete('/goods/:goodId', goodController.remove);
+
+router.get('/colors/', colorsController.getAll);
+router.get('/colors/:colorId', colorsController.getOne);
+
+
+app.use('/.netlify/functions/server', router);
 
 export const handler = serverless(app);
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,10 +3,11 @@ import * as dotenv from 'dotenv'
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+export const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
     dialectOptions: {
         ssl: {
             rejectUnauthorized: true,
         }
-    }
+    },
+    dialectModule: require('mysql2'),
 });
